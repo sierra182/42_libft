0076a75da0169faba6c557b598ee8096700c94fd
sierra182 <sierra-echo@msn.com>
2023-10-13 21:21:09 +0200

    norm lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 16:36:21 by svidot            #+#    #+#             */
-/*   Updated: 2023/10/13 21:02:59 by seblin           ###   ########.fr       */
+/*   Updated: 2023/10/13 21:14:41 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del) (void *))
 {
 	t_list	*new_l;
 	t_list	*link;
+	void	*cnt;
 
 	new_l = NULL;
 	link = NULL;
 	while (lst)
 	{
-		void* cnt = f(lst->content);
+		cnt = f(lst->content);
 		link = ft_lstnew(cnt);
 		if (!link)
 		{
-			del(cnt); //del(lst->content);
+			del(cnt);
 			ft_lstclear(&new_l, del);
-			//free(cnt);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_l, link);
 		lst = lst->next;
 	}
-	//ft_lstclear(&lst, del);
 	return (new_l);
 }
