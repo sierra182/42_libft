6c6da9a9d39ebde55ee37e36c3645d114480f66c
Sebastien Vidot <svidot@c3r1p2.42perpignan.fr>
2023-10-16 13:26:41 +0200

    clean .h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:15:23 by svidot            #+#    #+#             */
-/*   Updated: 2023/10/16 10:50:18 by svidot           ###   ########.fr       */
+/*   Updated: 2023/10/16 13:25:25 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,5 @@ void	ft_lstdelone(t_list *lst, void (*del) (void *));
 void	ft_lstclear(t_list **lst, void (*del) (void *));
 void	ft_lstiter(t_list *lst, void (*f) (void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del) (void *));
-//
+
 #endif
